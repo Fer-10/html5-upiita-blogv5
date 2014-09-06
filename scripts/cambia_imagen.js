e38090c3cff90ce2cc0880dcdf5d965e4bbe74f8
Daniela Aguirre <daniela@codeismo.com>
2014-09-06 12:37:07 -0500

    mono

--- scripts/cambia_imagen.js
+++ scripts/cambia_imagen.js
@@ -1,12 +1,20 @@
-$( function(){
+$(function() {
 	//obtener las cajas a manipular
 	var mono = $("#imagen-mono");
-	mono.mouseover( function(){
-		//cambiar la imagen
-		mono.attr("src", "estaticos/imagenes/ceo/otro_mono.jpg");
-	} );
-	
-	mono.mouseout( function(){
+	mono.mouseover(function() {
+		//1.-Ocultar la imagen
+		//un callback se ejecuta hasta que termino
+		//la animacion
+		mono.fadeOut("slow", function() {
+			//2.-Cambiar la imagen
+			//cambiar la imagen
+			mono.attr("src", "estaticos/imagenes/ceo/otro_mono.jpg");
+			//3.-Muestra la imagen
+			mono.fadeIn("slow");
+		});
+	});
+
+	mono.mouseout(function() {
 		mono.attr("src", "estaticos/imagenes/ceo/mono.jpg");
-	} );
-} );
+	});
+});
