33a61dc6bf31b085c21e03f7aa737ee4d3d16aa7
Daniela Aguirre <daniela@codeismo.com>
2014-09-13 10:00:34 -0500

    cerrrar

--- scripts/galeria.js
+++ scripts/galeria.js
@@ -1,19 +1,27 @@
-$( function(){
+$(function() {
 	/*** Obtener con jQuery las cajas a manipular ***/
-	var previews = $( "#contenedor-preview img" );
-	var principal = $( "#contenedor-imagen img" );
-	var descripcion_imagen = $( "#descripcion-imagen" );
-	
-	/** Escucho el evento click sobre las imagenes de 
+	var previews = $("#contenedor-preview img");
+	var principal = $("#contenedor-imagen img");
+	var descripcion_imagen = $("#descripcion-imagen");
+	var contenedor = $("#contenedor-imagen");
+	/** Escucho el evento click sobre las imagenes de
 	 *preview  **/
-	previews.click( function( evento ){
-		var imagen_clikeada = $( evento.target );
+	previews.click(function(evento) {
+
+		var imagen_clikeada = $(evento.target);
 		var ruta = imagen_clikeada.data("src-principal");
-		var descripcion = imagen_clikeada.data("descripcion");		
-		//cambiar el src de principal
-		principal.attr("src", ruta);
-		//cambiar el html de descripcion
-		descripcion_imagen.html( descripcion );
-	} );
-	
-} );
+		var descripcion = imagen_clikeada.data("descripcion");
+
+		//1.-oculta el contendor
+		//2.-cambia la imagen y la descripcion
+		//3.-muestra el contenedor
+		contenedor.fadeToggle("slow", function() {
+			//cambiar el src de principal
+			principal.attr("src", ruta);
+			//cambiar el html de descripcion
+			descripcion_imagen.html(descripcion);
+			//muestra de nuevo el contenedor
+			contenedor.fadeToggle("slow");
+		});
+	});
+});
---
+++ scripts/sociales.js
@@ -0,0 +1,10 @@
+$( function(){
+	//1.-obtener el enlace
+	//2.- escuchar el click sobre el enlace
+	//3.- ocultar el contenedor de imagenes (display: none)
+	
+	//hacer que al dar click si esta oculto se muestre y
+	//el enlace diga cerrar
+	//hacer que al dar click si esta mostrado se oculte y
+	//el enlace diga abrir (display: block)
+} );
