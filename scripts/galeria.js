bb9e9cd3fea51079497c752ef8634d7362267135
Daniela Aguirre <daniela@codeismo.com>
2014-09-13 09:28:34 -0500

    data galeria

--- scripts/galeria.js
+++ scripts/galeria.js
@@ -2,14 +2,18 @@ $( function(){
 	/*** Obtener con jQuery las cajas a manipular ***/
 	var previews = $( "#contenedor-preview img" );
 	var principal = $( "#contenedor-imagen img" );
-	var descripcion = $( "#descripcion-imagen" );
+	var descripcion_imagen = $( "#descripcion-imagen" );
 	
 	/** Escucho el evento click sobre las imagenes de 
 	 *preview  **/
 	previews.click( function( evento ){
 		var imagen_clikeada = $( evento.target );
-		var ruta = imagen_clikeada.attr("src");		
-		alert( ruta );
+		var ruta = imagen_clikeada.data("src-principal");
+		var descripcion = imagen_clikeada.data("descripcion");		
+		//cambiar el src de principal
+		
+		//cambiar el html de descripcion
+		
 	} );
 	
 } );
