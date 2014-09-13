d1e3df8f24cc3f61770d7f5f6e61def13782dfd0
Daniela Aguirre <daniela@codeismo.com>
2014-09-13 10:31:13 -0500

    cerrrar

--- scripts/sociales.js
+++ scripts/sociales.js
@@ -1,18 +1,24 @@
-$( function(){
+$(function() {
 	//1.-obtener el enlace
 	var enlace = $("#flota a");
 	//2.- escuchar el click sobre el enlace
 	var imagenes_sociales = $("#imagenes-sociales");
-	enlace.click( function(){
-		//3.- ocultar el contenedor de imagenes (display: none)	
-		imagenes_sociales.css("display", "none");
-		enlace.html("abrir");
+	enlace.click(function() {
+		//3.- ocultar el contenedor de imagenes (display: none)
+		if( imagenes_sociales.css("display") === "block" ) {
+			imagenes_sociales.css("display", "none");
+			enlace.html("abrir");
+			
+		} else {
+			imagenes_sociales.css("display", "block");
+			enlace.html("cerrar");
+		}
+
 		return false;
-	} );
-	
-	
+	});
+
 	//hacer que al dar click si esta oculto se muestre y
 	//el enlace diga cerrar
 	//hacer que al dar click si esta mostrado se oculte y
 	//el enlace diga abrir (display: block)
-} );
+});
